e3b936afe6743241b104580a7c90fe29dab20eb7
Dor <Dorpel55@gmail.com>
2026-03-18 18:32:28 +0200

    edited the timing

--- RISC-V/tb_RiscV_Top.sv
+++ RISC-V/tb_RiscV_Top.sv
@@ -4,28 +4,22 @@ module tb_RiscV_Top();
     logic clk;
     logic rst;
 
-    // Instance of your Top Level
     RiscV_Top dut (
         .clk(clk),
         .rst(rst)
     );
 
-    // Clock generation: 10ns period (100MHz)
+    //10ns period (100MHz)
     always #5 clk = ~clk;
 
     initial begin
-        // Initialize signals
         clk = 0;
-        rst = 0; // Start with reset active (Low)
+        rst = 0;
 
-        // Hold reset for 20ns
         #20;
-        rst = 1; // Release reset
+        rst = 1;
 
-        // Run for a few cycles to see the program execute
         #200;
-
-        $display("Simulation Finished. Check the Waveform window!");
         $stop;
     end
 
